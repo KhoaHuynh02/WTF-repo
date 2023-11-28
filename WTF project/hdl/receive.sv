e7b0a033d26f3b417902631e7e00960e83c5d4ac
Khang Le <khangle@DHCP-POOL-18-25-25-074.MIT.EDU>
2023-11-28 13:14:16 -0500

    added backup for transmit.sv

--- WTF project/hdl/receive.sv
+++ WTF project/hdl/receive.sv
@@ -1,5 +1,17 @@
 `timescale 1ns / 1ps
 `default_nettype none // prevents system from inferring an undeclared logic (good practice)
 
+// assuming that the information comes in at 12khz at 8 bits depth
+module receive (
+    input wire clk_in,
+    input wire rst_in,
+    input wire record_done,
+    input wire [7:0] audio_in,
+    input wire audio_valid_in,
+    output logic valid_out,
+    output logic out
+);
+
+endmodule
 
 `default_nettype wire
